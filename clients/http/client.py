01dc04c2b3cf9730ad0959c18ce26333e29323ca
ilua2013 <ilua.2013@inbox.ru>
2025-11-17 12:36:52 +0300

    Практикуемся в реализации HTTP API клиентов

--- clients/http/client.py
+++ clients/http/client.py
@@ -1,3 +1,5 @@
+from typing import Any
+
 from httpx import Client, URL, Response, QueryParams
 
 
@@ -14,9 +16,19 @@ class HTTPClient:
     def get(self, url: URL | str, params: QueryParams | None = None) -> Response:
         """
         Выполняет GET-запрос.
-        
+
         :param url: URL-адрес эндпоинта.
         :param params: GET-параметры запроса (например, ?key=value).
         :return: Объект Response с данными ответа.
         """
         return self.client.get(url, params=params)
+    
+    def post(self, url: str, json: Any | None = None) -> Response:
+        """
+        Выполняет POST-запрос.
+
+        :param url: URL-адрес эндпоинта.
+        :param json: Данные в формате JSON.
+        :return: Объект Response с данными ответа.
+        """
+        return self.client.post(url=url, json=json)
